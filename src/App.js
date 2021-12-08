fb428dc77996f445c1834d271e3f37a26f8b81f6
MireilleNyoubissi <nmireillemat@yahoo.fr>
2021-12-08 02:00:31 +0000

    added css styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React from "react";
+import JobsProfiles from "./data/jobsProfiles.json"
 import './App.css';
+import JobCard from "./JobCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="all">
+        <JobCard data = {JobsProfiles}/>
     </div>
   );
 }
---
+++ src/JobCard.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+function JobCard(props){
+    return (
+      <div className="card-container">
+        {props.data.map((jobProfile, index) => (
+          <div className="card">
+            <img
+              key={index + 0.5}
+              src={jobProfile.logo}
+              className="card-img-top"
+              alt="company's logo"
+            />
+            <div className="card-body">
+              <h5 className="card-title" key={index + 0.2}>
+                {jobProfile.company}
+
+                {jobProfile.new ? (
+                  <span className="newsStyle">New!</span>
+                ) : null}
+
+                {jobProfile.new ? (
+                  <span className="featuredStyle">FEATURED</span>
+                ) : null}
+              </h5>
+              <div className="ul-wrapper">
+                <div className="left">
+                  <h6 className="card-text" key={index + 0.1}>
+                    {jobProfile.position}
+                  </h6>
+                  <ul className="list1">
+                    <li className="card-text" key={index + 0.23}>
+                      {jobProfile.postedAt}
+                    </li>
+                    <li className="card-text" key={index + 0.33}>
+                      {jobProfile.contract}
+                    </li>
+                    <li className="card-text" key={index + 0.15}>
+                      {jobProfile.location}
+                    </li>
+                  </ul>
+                </div>
+                <div className="right">
+                  <ul className="list2">
+                    <li>{jobProfile.role}</li>
+                    <li>{jobProfile.level}</li>
+                    {jobProfile.tools.map((v) => (
+                      <li>{v}</li>
+                    ))}
+                    {jobProfile.languages.map((v) => (
+                      <li>{v}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+}
+
+export default JobCard;
